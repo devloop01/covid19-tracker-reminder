c56aa98a41cca5e747b6af2d919f35c274c9f9a0
devloop01 <destrodev123@gmail.com>
2020-04-18 00:44:50 +0530

    updated API version

--- js/tracker.js
+++ js/tracker.js
@@ -1,6 +1,6 @@
 document.addEventListener(
 	"DOMContentLoaded",
-	function() {
+	function () {
 		// -----------------------------------------------------------------------
 		// --------------------- TRACKER SCRIPTS ---------------------------------------------
 		// -------------------------------------------------------
@@ -8,8 +8,10 @@ document.addEventListener(
 		const refreshButton = document.querySelector("button.refresh");
 		const locationInputEl = document.querySelector(".tracker-content #location");
 		const locationsListEl = document.querySelector(".tracker-content #locations-list");
-		const trackerAppActionsContainerEl = document.querySelector(".tracker-content .app__body--actions");
-		const API_URL = "https://corona.lmao.ninja";
+		const trackerAppActionsContainerEl = document.querySelector(
+			".tracker-content .app__body--actions"
+		);
+		const API_URL = "https://corona.lmao.ninja/v2";
 
 		let animating = false;
 		let allLocationNames = [];
@@ -29,14 +31,16 @@ document.addEventListener(
 			showedLastUpdatedTime = false;
 
 		function initCoronaTracker() {
-			chrome.storage.local.get(["searchedLocation"], storage => {
+			chrome.storage.local.get(["searchedLocation"], (storage) => {
 				if (storage.searchedLocation != undefined) {
 					searchedLocation = storage.searchedLocation;
 				}
 				locationInputEl.value = searchedLocation;
 
 				while (trackerAppActionsContainerEl.firstChild.tagName != "BUTTON") {
-					trackerAppActionsContainerEl.removeChild(trackerAppActionsContainerEl.firstChild);
+					trackerAppActionsContainerEl.removeChild(
+						trackerAppActionsContainerEl.firstChild
+					);
 				}
 
 				changeRefreshButtonAnimationState(dataLoaded == false);
@@ -56,12 +60,15 @@ document.addEventListener(
 				showInfoIfLocationIsNotValid();
 			} else if (searchedLocation.toLowerCase() == locationInputEl.value.toLowerCase()) {
 				showInfoIfDataIsUpdated();
-			} else if (searchedLocation.toLowerCase() != locationInputEl.value.toLowerCase() && isValidLocation) {
+			} else if (
+				searchedLocation.toLowerCase() != locationInputEl.value.toLowerCase() &&
+				isValidLocation
+			) {
 				fetchLocationDetails(locationInputEl.value);
 			}
 		});
 
-		locationInputEl.addEventListener("keydown", e => {
+		locationInputEl.addEventListener("keydown", (e) => {
 			const ENTER_KEYCODE = 13;
 			if (e.keyCode == ENTER_KEYCODE && isValidLocation == true) {
 				if (searchedLocation.toLowerCase() != locationInputEl.value.toLowerCase()) {
@@ -72,7 +79,11 @@ document.addEventListener(
 				}
 				searchedLocation = locationInputEl.value;
 				chrome.storage.local.set({ searchedLocation: locationInputEl.value });
-			} else if (e.keyCode == ENTER_KEYCODE && isValidLocation == false && !locationInputEl.value.length == 0) {
+			} else if (
+				e.keyCode == ENTER_KEYCODE &&
+				isValidLocation == false &&
+				!locationInputEl.value.length == 0
+			) {
 				showInfoIfLocationIsNotValid();
 				locationInputEl.classList.add("shake");
 				removeClassAfterAnimationCompletes(locationInputEl, "shake");
@@ -97,11 +108,14 @@ document.addEventListener(
 			showEffectedChange(location.toLowerCase() == "world" ? "world" : location);
 			if (location.length == 0) return;
 
-			let url = location.toLowerCase() == "world" ? `${API_URL}/all` : `${API_URL}/countries/${location.toLowerCase()}`;
+			let url =
+				location.toLowerCase() == "world"
+					? `${API_URL}/all`
+					: `${API_URL}/countries/${location.toLowerCase()}`;
 			console.log("fetching", url);
 			fetch(url)
-				.then(res => res.json())
-				.then(data => {
+				.then((res) => res.json())
+				.then((data) => {
 					updateData(data);
 					updateApp(dataObj);
 					dataLoaded = true;
@@ -132,21 +146,36 @@ document.addEventListener(
 
 			console.log("updating App");
 
-			animateValue(confirmedCasesEl, parseInt(confirmedCasesEl.innerHTML), data.infected, counterStepAnimationDuration);
-			animateValue(recoveredEl, parseInt(recoveredEl.innerHTML), data.recovered, counterStepAnimationDuration);
-			animateValue(deathsEl, parseInt(deathsEl.innerHTML), data.deaths, counterStepAnimationDuration);
+			animateValue(
+				confirmedCasesEl,
+				parseInt(confirmedCasesEl.innerHTML),
+				data.infected,
+				counterStepAnimationDuration
+			);
+			animateValue(
+				recoveredEl,
+				parseInt(recoveredEl.innerHTML),
+				data.recovered,
+				counterStepAnimationDuration
+			);
+			animateValue(
+				deathsEl,
+				parseInt(deathsEl.innerHTML),
+				data.deaths,
+				counterStepAnimationDuration
+			);
 		}
 
 		function addLocationsToDatalist(dataList) {
 			fetch(`${API_URL}/countries/`)
-				.then(res => res.json())
-				.then(data => data.map(d => d.country))
-				.then(countryNamesList => {
+				.then((res) => res.json())
+				.then((data) => data.map((d) => d.country))
+				.then((countryNamesList) => {
 					allLocationNames.push(...countryNamesList, "World");
 				})
 				.then(() => {
 					removeChild(dataList);
-					allLocationNames.forEach(country => {
+					allLocationNames.forEach((country) => {
 						let option = document.createElement("option");
 						option.innerHTML = country;
 						dataList.appendChild(option);
@@ -159,9 +188,9 @@ document.addEventListener(
 
 		function updateMostEffected() {
 			fetch(`${API_URL}/countries`)
-				.then(res => res.json())
-				.then(data =>
-					data.map(d => {
+				.then((res) => res.json())
+				.then((data) =>
+					data.map((d) => {
 						return {
 							country: d.country,
 							cases: d.cases,
@@ -169,7 +198,7 @@ document.addEventListener(
 						};
 					})
 				)
-				.then(listOfData => {
+				.then((listOfData) => {
 					mostInfectedCountryData = listOfData.sort((a, b) => b.cases - a.cases)[0];
 					mostDeathsCountryData = listOfData.sort((a, b) => b.deaths - a.deaths)[0];
 				})
@@ -185,19 +214,23 @@ document.addEventListener(
 
 		function updateLastUpdatedTime() {
 			return fetch(`${API_URL}/all`)
-				.then(res => res.json())
-				.then(data => data.updated)
-				.then(updatedTime => {
+				.then((res) => res.json())
+				.then((data) => data.updated)
+				.then((updatedTime) => {
 					let localeDate = new Date(updatedTime).toLocaleDateString(),
 						localeTime = new Date(updatedTime).toLocaleTimeString(),
 						timeIn12Format = change24To12HourFormat(localeTime, ":");
 					lastUpdated = `${localeDate} | ${timeIn12Format}`;
 					if (showedLastUpdatedTime == false) {
-						showInfoText(trackerAppActionsContainerEl, `Last updated : ${lastUpdated}`, 2500);
+						showInfoText(
+							trackerAppActionsContainerEl,
+							`Last updated : ${lastUpdated}`,
+							2500
+						);
 						showedLastUpdatedTime = true;
 					}
 				})
-				.catch(err => {
+				.catch((err) => {
 					console.log(err, "could not update last updated time");
 				});
 		}
@@ -208,7 +241,12 @@ document.addEventListener(
 			refreshButton.querySelector(".icon.refresh").classList.remove("loading");
 			refreshButton.querySelector(".icon.refresh").style.display = "none";
 			refreshButton.querySelector(".icon.cross").style.display = "";
-			showInfoText(trackerAppActionsContainerEl, "check your connection!", 2000, "rgba(255, 0, 0, 1)");
+			showInfoText(
+				trackerAppActionsContainerEl,
+				"check your connection!",
+				2000,
+				"rgba(255, 0, 0, 1)"
+			);
 
 			refreshButton.addEventListener("click", () => {
 				refreshButton.querySelector(".btn-info").innerHTML = "refresh";
@@ -228,7 +266,12 @@ document.addEventListener(
 
 		function showInfoIfLocationIsNotValid() {
 			console.log("Location not found");
-			showInfoText(trackerAppActionsContainerEl, "Location not found!", 1500, "rgba(255, 0, 0, 1)");
+			showInfoText(
+				trackerAppActionsContainerEl,
+				"Location not found!",
+				1500,
+				"rgba(255, 0, 0, 1)"
+			);
 		}
 
 		function showInfoText(parent, text, duration, color) {
@@ -242,7 +285,7 @@ document.addEventListener(
 			el.className = "info-text";
 			el.classList.add("show");
 			parent.insertAdjacentElement("afterbegin", el);
-			setTimeout(function() {
+			setTimeout(function () {
 				el.classList.remove("show");
 			}, duration);
 		}
@@ -250,35 +293,42 @@ document.addEventListener(
 		function showEffectedChange(location) {
 			let todayCases, todayRecovered, todayDeaths;
 			if (location == "world") {
-				chrome.storage.local.get(["totalInfected", "totalRecovered", "totalDeaths"], storage => {
-					if (storage.totalInfected != 0 && storage.totalRecovered != 0 && storage.totalDeaths != 0) {
-						let prevData, newData;
-						fetch(`${API_URL}/all`)
-							.then(res => res.json())
-							.then(data => {
-								prevData = {
-									infected: storage.totalInfected,
-									recovered: storage.totalRecovered,
-									deaths: storage.totalDeaths,
-								};
-								newData = {
-									infected: data.cases,
-									recovered: data.recovered,
-									deaths: data.deaths,
-								};
-							})
-							.then(() => {
-								todayCases = newData.infected - prevData.infected;
-								todayRecovered = newData.recovered - prevData.recovered;
-								todayDeaths = newData.deaths - prevData.deaths;
-							})
-							.then(showChange);
+				chrome.storage.local.get(
+					["totalInfected", "totalRecovered", "totalDeaths"],
+					(storage) => {
+						if (
+							storage.totalInfected != 0 &&
+							storage.totalRecovered != 0 &&
+							storage.totalDeaths != 0
+						) {
+							let prevData, newData;
+							fetch(`${API_URL}/all`)
+								.then((res) => res.json())
+								.then((data) => {
+									prevData = {
+										infected: storage.totalInfected,
+										recovered: storage.totalRecovered,
+										deaths: storage.totalDeaths,
+									};
+									newData = {
+										infected: data.cases,
+										recovered: data.recovered,
+										deaths: data.deaths,
+									};
+								})
+								.then(() => {
+									todayCases = newData.infected - prevData.infected;
+									todayRecovered = newData.recovered - prevData.recovered;
+									todayDeaths = newData.deaths - prevData.deaths;
+								})
+								.then(showChange);
+						}
 					}
-				});
+				);
 			} else {
 				fetch(`${API_URL}/countries/${location}`)
-					.then(res => res.json())
-					.then(data => {
+					.then((res) => res.json())
+					.then((data) => {
 						todayCases = data.todayCases;
 						todayDeaths = data.todayDeaths;
 					})
@@ -291,12 +341,14 @@ document.addEventListener(
 						recoveredChangeEl = document.getElementById("recovered-change"),
 						deathsChangeEl = document.getElementById("deaths-change");
 
-					if (todayCases != 0 && todayCases != undefined) addClassAndChangeHTML(infectedChangeEl, todayCases, "show");
+					if (todayCases != 0 && todayCases != undefined)
+						addClassAndChangeHTML(infectedChangeEl, todayCases, "show");
 					if (todayRecovered != 0 && todayRecovered != undefined)
 						addClassAndChangeHTML(recoveredChangeEl, todayRecovered, "show");
-					if (todayDeaths != 0 && todayDeaths != undefined) addClassAndChangeHTML(deathsChangeEl, todayDeaths, "show");
+					if (todayDeaths != 0 && todayDeaths != undefined)
+						addClassAndChangeHTML(deathsChangeEl, todayDeaths, "show");
 
-					setTimeout(function() {
+					setTimeout(function () {
 						infectedChangeEl.classList.remove("show");
 						recoveredChangeEl.classList.remove("show");
 						deathsChangeEl.classList.remove("show");
@@ -365,7 +417,7 @@ document.addEventListener(
 
 		function removeClassAfterAnimationCompletes(el, className) {
 			let elStyles = window.getComputedStyle(el);
-			setTimeout(function() {
+			setTimeout(function () {
 				el.classList.remove(className);
 			}, +elStyles.animationDuration.replace("s", "") * 1000);
 		}
